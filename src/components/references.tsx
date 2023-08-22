a3c395181fb0a9c5f3dd480d4d60178fb8087bd2
Stitch <97808356+stitcharg@users.noreply.github.com>
2023-08-22 20:13:22 -0300

    Arreglados los acentos y agregadas las referencias

--- src/components/references.tsx
+++ src/components/references.tsx
@@ -5,24 +5,33 @@ const References = () => {
 			<div className="items-center grid-flow-row">
 				<h2 className="text-lg font-bold">Referencias:</h2>
 				<ul className="list-disc pl-10">
-					<li>
-						<a href="https://pubmed.ncbi.nlm.nih.gov/1359444/" className="flex" target="out">Repeated administration of MDMA causes transient down-regulation of serotonin 5-HT2 receptors
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ml-1">
-								<path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
-								<path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
-							</svg>
-
-						</a>
-					</li>
-					<li><a href="https://argenpills.org/showthread.php?tid=1086" className="flex" target="out">Foro AP: No respetar los descansos
-						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ml-1">
-							<path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
-							<path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
-						</svg>
-					</a></li>
+					<ReferenceLink name="Repeated administration of MDMA causes transient down-regulation of serotonin 5-HT2 receptors" link="https://pubmed.ncbi.nlm.nih.gov/1359444/" />
+					<ReferenceLink name="No respectar los descansos" link="https://argenpills.org/showthread.php?tid=1086" />
+					<ReferenceLink name="C&oacute;mo consumir &eacute;xtasis de forma segura" link="https://argenpills.org/showthread.php?tid=961" />
+					<ReferenceLink name="Informaci&oacute;n basica sobre MDMA" link="https://argenpills.org/showthread.php?tid=413" />
+					<ReferenceLink name="Consumir dos d&iacute;as seguidos" link="https://argenpills.org/showthread.php?tid=5" />
+					<ReferenceLink name="Qu&eacute; es la tolerancia?" link="https://argenpills.org/showthread.php?tid=2468" />
+					<ReferenceLink name="Long-Term Comedown" link="https://argenpills.org/showthread.php?tid=276" />
 				</ul>
 			</div>
 		</footer>
+	);
+}
+
+interface iReferenceProps {
+	link: string,
+	name: string
+}
+const ReferenceLink = (props:iReferenceProps) => {
+	return (
+		<li>
+			<a href={props.link} className="flex" target="out">{props.name}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 ml-1">
+				<path fillRule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clipRule="evenodd" />
+				<path fillRule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clipRule="evenodd" />
+			</svg>
+			</a>
+		</li>
 	);
 }
 
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -11,8 +11,8 @@ const Timeline = () => {
 					title: "Hora 0",
 					cardTitle: "Toma inicial",
 					timelineContent: <div>Empieza nuestra aventura, y tomamos la sustancia.
-						Recuerden que el formato de la sustancia no varia y puede ser pastillas o en formato cristal. La droga, en definitiva, es la misma. Asumimos que pasaron, al menos, 90 dias desde el ultimo
-						consumo de extasis, o es tu primera vez
+						Recuerden que el formato de la sustancia no var&iacute;a y puede ser pastillas o en formato cristal. La droga, en definitiva, es la misma. Asumimos que pasaron, al menos, 90 dias desde el &uacute;ltimo
+						consumo de extasis, o es tu primera vez.
 						<Full caption="Nuestro tanquecito esta lleno de serotonina"></Full>
 					</div>
 				},
@@ -20,10 +20,10 @@ const Timeline = () => {
 					title: "Hora +2",
 					cardTitle: "En la cresta de la ola",
 					timelineContent: <div>
-						Estamos en el pico del viaje, todo es amor y felicidad, queremos que no se termine nunca
-						Recordemos hidratarnos, comer cositas dulces, frutas (si tenemos disponibles), charlar con nuestros amigos y amigas, darnos besos y sentir la musica. Todo es perfecto!
+						Estamos en el pico del viaje, todo es amor y felicidad, queremos que no se termine nunca!
+						Recordemos hidratarnos, comer cositas dulces, frutas (si tenemos disponibles), charlar con nuestros amigos y amigas, darnos besos y sentir la m&uacute;sica. Todo es perfecto!
 						<br />
-						Si estas pensando en redosificar, recorda hacerlo con la <strong>mitad de la dosis original</strong> (si tomaste una pastilla, toma 1/2 mas, si tomaste 1/2, 1/4, etc.). Respetar esto es importante
+						Si est&aacute;s pensando en redosificar, recorda hacerlo con la <strong>mitad de la dosis original</strong> (si tomaste una pastilla, toma 1/2 mas, si tomaste 1/2, 1/4, etc.). Respetar esto es importante
 						para evitar la neurotoxicidad, y cada vez que repitas la toma, vas a sentir menos sensaciones agradables.
 						<FullToEmpty caption="Nuestro tanquecito se va vaciando a medida que la sustancia va circulando en nuestro sistema" />
 					</div>
@@ -42,10 +42,10 @@ const Timeline = () => {
 					title: "Dia +2",
 					cardTitle: "2 dias despues",
 					cardSubtitle: `Tuesday Blues`,
-					timelineContent: <div>Ya pasaron 2 dias de la toma, estamos empezando a volver a la normalidad, pero podemos sentir un bajon animico muy grande. Se le llama "Tuesday Blues" en ingles ("Martes triste", porque usualmente,
-						si consumimos el dia sabado, hoy seria martes),
-						y es muy comun, dado que el cerebro esta todavia recuperandose. Podes sentir irritabilidad, tristeza, depresion, poca esperanza. Trat&aacute; de no pelearte con nadie de tu familia, y tene en cuenta
-						que es un proceso quimico y va a pasar pronto.
+					timelineContent: <div>Ya pasaron 2 d&iacute;as de la toma, estamos empezando a volver a la normalidad, pero podemos sentir un baj&oacute;n an&iacute;mico muy grande. Se le llama "Tuesday Blues" en ingles ("Martes triste", porque usualmente,
+						si consumimos el dia s&aacute;bado, hoy seria martes),
+						y es muy comun, dado que el cerebro esta todavia recuper&aacute;ndose. Pod&eacute;s sentir irritabilidad, tristeza, depresi&oacute;n, poca esperanza. Trat&aacute; de no pelearte con nadie de tu familia, y ten&eacute; en cuenta
+						que es un proceso qu&iacute;mico y va a pasar pronto.
 						<Empty caption="Nuestro tanquecito sigue vacio"></Empty>
 					</div>
 				},
@@ -53,10 +53,10 @@ const Timeline = () => {
 					title: "Dia +4",
 					cardTitle: "4 dias despues",
 					cardSubtitle: `(Casi) normales`,
-					timelineContent: <div>Pasaron 4 dias desde que consumimos. Nos sentimos bastante mejor, dormimos bien, tenemos buen animo, podemos concentrarnos, casi que parece que no tomamos nada el finde, y estamos
+					timelineContent: <div>Pasaron 4 dias desde que consumimos. Nos sentimos bastante mejor, dormimos bien, tenemos buen &aacute;nimo, podemos concentrarnos, casi que parece que no tomamos nada el finde, y estamos
 						listos para volver al ruedo, pero nuestro cerebro todavia se est&aacute; recuperando!
-						<br />Si sentis que no te est&aacute;s recuperando, o est&aacute;s asustado o preocupado por algun sintoma, te recomendamos acudir al medico! Si no te anim&aacute;s, podes dejar un mensaje en el foro de Argenpills, tal vez
-						encuentres alguna respuesta ahi, pero <strong>recorda que no somos medicos ni reemplazmos su funcion</strong>!
+						<br />Si sentis que no te est&aacute;s recuperando, o est&aacute;s asustado o preocupado por algun sintoma, te recomendamos acudir al medico! Si no te anim&aacute;s, podes dejar un mensaje en el <a href="http://argenpills.org">foro de Argenpills</a>, 
+						tal vez encuentres alguna respuesta ahi, pero <strong>record&aacute; que no somos m&eacute;dicos ni reemplazmos su funci&oacute;n</strong>!.
 						<Empty caption="Nuestro tanquecito sigue vacio! Aunque nos sintamos mejor"></Empty>
 					</div>
 				},
@@ -64,8 +64,8 @@ const Timeline = () => {
 					title: "Dia +20",
 					cardTitle: "20 dias despues",
 					cardSubtitle: `Lo que no se ve`,
-					timelineContent: <div>Pasaron 20 dias desde la toma de extasis. Tal vez nosotros no nos acordemos, pero nuestro cerebro, recien ahora, puede volver a generar la hormona necesaria para la produccion de
-						serotonina en nuestro cerebro! No hay forma de acelerar este proceso, el cerebro es muy elastico pero necesita mucho tiempo para volver al equilibrio quimico
+					timelineContent: <div>Pasaron 20 dias desde la toma de &eacute;xtasis. Tal vez nosotros no nos acordemos, pero nuestro cerebro, reci&eacute;n ahora, puede volver a generar la hormona necesaria para la producci&oacute;n de
+						serotonina en nuestro cerebro! No hay forma de acelerar este proceso, el cerebro es muy el&aacute;stico pero necesita mucho tiempo para volver al equilibrio qu&iacute;mico.
 						<Filling caption="Nuestro tanquecito empieza a llenarse nuevamente, lentamente"></Filling>
 					</div>
 				},
@@ -73,7 +73,7 @@ const Timeline = () => {
 					title: "Dia +30",
 					cardTitle: "30 dias despues",
 					cardSubtitle: `Estamos listos para otro round... o no?`,
-					timelineContent: <div>Ya pasaron 30 dias desde nuestra toma de extasis. Esperamos el tiempos suficiente para que la serotonina empiece a recuperarse en nuestro cerebro
+					timelineContent: <div>Ya pasaron 30 d&iacute;as desde nuestra toma de &eacute;xtasis. Esperamos el tiempos suficiente para que la serotonina empiece a recuperarse en nuestro cerebro.
 						<Half caption="Nuestro tanquecito pudo llenarse durante 10 dias, pero no llega a llenarse del todo"></Half>
 					</div>
 				},
@@ -82,7 +82,7 @@ const Timeline = () => {
 					title: "Dia +60",
 					cardTitle: "60 dias despues (o mas!)",
 					cardSubtitle: `Cosas buenas les esperan a los que esperan`,
-					timelineContent: <div>Pasaron <strong>60 dias (o mas!)</strong> desde la ultima toma de extasis. Nuestro cerebro esta casi recuperado!
+					timelineContent: <div>Pasaron <strong>60 d&iacute;as (o m&aacute;s!)</strong> desde la &uacute;ltima toma de &eacute;xtasis. Nuestro cerebro est&aacute; casi recuperado!
 						Si tenemos ganas, podemos repetir la toma, respetando la dosis recomendada (maximo 120-150mg) de forma
 						segura, teniendo las precauciones y recaudos suficientes
 						<Full caption="Nuestro tanquecito esta lleno de serotonina nuevamente!"></Full>
@@ -93,10 +93,10 @@ const Timeline = () => {
 					title: "Dia +90",
 					cardTitle: "90 dias despues (o mas!)",
 					cardSubtitle: `Recuperado completamente!`,
-					timelineContent: <div>Pasaron <strong>90 dias (o mas!)</strong> desde la ultima toma de extasis. Nuestro cerebro est&aacute; recuperado! (al menos a nivel quimico)
+					timelineContent: <div>Pasaron <strong>90 d&iacute;as (o m&aacute;s!)</strong> desde la ultima toma de &eacute;xtasis. Nuestro cerebro est&aacute; recuperado! (al menos a nivel qu&iacute;mico)
 						Es el tiempo ideal de espera entre consumos de extasis, si queremos disminuir los efectos adversos y problemas a futuro.
 						Si tenemos ganas, podemos repetir la toma, respetando la dosis recomendada (maximo 120-150mg) de forma
-						segura, teniendo las precauciones y recaudos suficientes
+						segura, teniendo las precauciones y recaudos suficientes.
 						<Full caption="Nuestro tanquecito esta lleno de serotonina nuevamente!"></Full>
 					</div>,
 				},
