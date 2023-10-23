0c6580c0993d7530337464db649f30a11540ab2b
Stitch <97808356+stitcharg@users.noreply.github.com>
2023-10-23 16:45:10 -0300

    Removido Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react(),
-		VitePluginRadar({
-			// Google Analytics tag injection
-			analytics: {
-				id: 'G-7V9PR3110R',
-			},
-			})
+		react()
 	],
 })
