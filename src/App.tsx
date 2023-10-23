6d7400735432805692218d39f8bd698bd4841577
Stitch <97808356+stitcharg@users.noreply.github.com>
2023-10-23 16:40:14 -0300

    Agregado soporte para Clarity

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,18 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import AboutUs from './pages/about-us'
 import Homepage from './pages/homepage'
+import { useEffect } from 'react'
+import { clarity } from 'react-microsoft-clarity'
 
 function App() {
+
+	if (!import.meta.env.DEV)
+		useEffect(() => {
+			clarity.init(
+				import.meta.env.VITE_CLARITY_ID
+			);
+		}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
