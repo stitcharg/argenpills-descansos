bf563511b2357ff5b221461e7248c231aef4fb97
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-02-04 17:45:33 -0300

    Arreglado el path (ahora si)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.
     use(initReactI18next)
+    .use(LanguageDetector)
     .use(HttpApi)
     .init({
-        lng: "es",
-        fallbackLng: "en",
         detection: {
-            order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
-            caches: ['cookie'],
+            order: ['path']
         },
         interpolation: {
             escapeValue: false,
