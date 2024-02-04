e8b7752508d6d6ab86319f1908ba129409a55ef9
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-02-04 13:07:46 -0300

    Le sacamos la autodeteccion y forzamos es cuando carga

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,13 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpApi from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n.
     use(initReactI18next)
     .use(HttpApi)
-    .use(LanguageDetector)
     .init({
-        fallbackLng: "es",
+        lng: "es",
+        fallbackLng: "en",
         detection: {
             order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
             caches: ['cookie'],
