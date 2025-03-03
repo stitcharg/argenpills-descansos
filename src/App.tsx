46a6e8e1d3abfbed0de8cb0d485502b29ec2aa14
Stitch <97808356+stitcharg@users.noreply.github.com>
2025-03-03 13:53:06 -0300

    Poniendo /es/ como default y limpiando gitIgnore

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import AboutUs from './pages/about-us'
 import Homepage from './pages/homepage'
 import { useEffect, Suspense } from 'react'
 import { clarity } from 'react-microsoft-clarity'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+	const { i18n } = useTranslation();
+
+	useEffect(() => {
+		if (i18n.isInitialized) {
+			i18n.changeLanguage('es');
+		}
+	}, [i18n.isInitialized]);
 
 	if (!import.meta.env.DEV)
 		useEffect(() => {
@@ -26,8 +34,17 @@ function App() {
 					<Route path="/es" element={<Homepage language='es' />} />
 					<Route path="/es/quienes-somos" element={<AboutUs language='es' />} />
 
-					{/* Redirect to Spanish home as a fallback */}
-					<Route path="*" element={<Navigate to="/es" replace />} />
+					{/* Wait for i18n to be ready before redirecting */}
+					<Route
+						path="*"
+						element={
+							i18n.isInitialized ? (
+								<Navigate to="/es" replace />
+							) : (
+								<div>Cargando / Loading...</div>
+							)
+						}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</Suspense>
