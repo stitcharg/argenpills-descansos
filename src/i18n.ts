7efeba66f6fa89911d6499508a537ed04d776d72
Stitch <97808356+stitcharg@users.noreply.github.com>
2025-03-04 10:55:38 -0300

    Mas pruebas, y upgrade de librerias

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,14 +3,17 @@ import { initReactI18next } from "react-i18next";
 import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18n.
-	use(initReactI18next)
-	.use(LanguageDetector)
+const languageDetector = new LanguageDetector(null, {
+	order: ['path'],
+	lookupFromPathIndex: 0,
+});
+
+i18n
+	.use(initReactI18next)
+	.use(languageDetector)
 	.use(HttpApi)
 	.init({
-		detection: {
-			order: ['path']
-		},
+		fallbackLng: 'es',
 		interpolation: {
 			escapeValue: false,
 		},
